ffefd5185737e7fc24383d43483ba912084f2334
palec87 <palec87@users.noreply.huggingface.co>
2026-06-13 14:04:39 +0200

    version bump

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ LANGUAGE_OPTIONS = {
     "Deutsch": "de",
 }
 
-_VERSION = 0.1
+_VERSION = 0.2
 STYLE_OPTIONS = ["minimal", "newspaper", "watercolor", "retro"]
 MAX_SEED = 2**31 - 1
 MAX_IMAGE_SIZE = 512
